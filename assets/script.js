f4fdffa15c670df387ef356288ad14b6a625927d
Ibtissam Rémond <ibtissam.soufiani@gmail.com>
2023-07-24 12:51:05 +0200

    ajout_index4

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,25 @@
 const slides = [
 	{
 		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>",
-		"doty":`
-			<button class="dot dot_selected" id="dot1"></button>
-			<button class="dot" id="dot2"></button>
-			<button class="dot" id="dot3"></button>
-			<button class="dot" id="dot4"></button>	
-			`
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 		
 			
 		
 	},
 	{
 		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>",
-		"doty":`
-			<button class="dot" id="dot1"></button>
-			<button class="dot dot_selected" id="dot2"></button>
-			<button class="dot" id="dot3"></button>
-			<button class="dot" id="dot4"></button>	
-			`
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		
 	},
 	{
 		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>",
-		"doty":`
-			<button class="dot" id="dot1"></button>
-			<button class="dot" id="dot2"></button>
-			<button class="dot dot_selected" id="dot3"></button>
-			<button class="dot" id="dot4"></button>	
-			`
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		
 	},
 	{
 		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>",
-		"doty":`
-			<button class="dot" id="dot1"></button>
-			<button class="dot" id="dot2"></button>
-			<button class="dot" id="dot3"></button>
-			<button class="dot dot_selected" id="dot4"></button>	
-			`
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		
 	}
 ]
 
@@ -54,7 +33,7 @@ const flechGauche = document.querySelector(".arrow_left");
 
 /******dots */
 
-const dotes = document.querySelector(".dots");
+const dotes = document.querySelectorAll(".dot");
 
 
 
@@ -81,7 +60,6 @@ flechGauche.addEventListener("click", function () {
 });
 
 
-/*dotes.addEventListener("click", function () {});*/
 
 
 function changeSlide()
@@ -92,34 +70,27 @@ function changeSlide()
 	document.getElementById("slide").src =  "./assets/images/slideshow/" + slideCourante.image;
 	document.getElementById("text").innerHTML =   slideCourante.tagLine;
 
-	/*for (var i=0 ;  i<slides.length; i++ )
+	for (var i=0 ;  i<dotes.length; i++ )
 	{
-		
-	}*/
+		var dot = dotes[i];
+		if (i===indexCourant)
+		{ 
+			
+			dot.classList.add("dot_selected");
+		}
+		else
+		{
+			dot.classList.remove("dot_selected");
+		}
+	}
 
-	cdot();
-
-}
-
-window.onload=init;
-
-function init()
-{
-	cdot();
-}
-
-function cdot()
-{
-	var slidedot = slides[indexCourant];
-
-	document.getElementById("dotsId").innerHTML = slidedot.doty;
-
-	/*for (var i=0 ;  i<slides.length; i++ )
-	{
-		
-	}*/
+	
 
 }
+
+
+
+
 
 
 
